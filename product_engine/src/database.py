6ff39893d76d57e3d2ef4227624704642ae30726
CallMeDron <Andreyfrendak@yandex.ru>
2024-01-17 15:59:40 +0300

    Initial commit

--- product_engine/src/database.py
+++ product_engine/src/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker, Session
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:postgres@172.17.0.1:5432/product_engine"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/product_engine"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionMaker = sessionmaker(autocommit=False, autoflush=False, bind=engine)
